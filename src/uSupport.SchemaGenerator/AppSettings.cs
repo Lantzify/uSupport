c5481fe084bc06b072a3ed37a0731296d5cd63df
Lantzify <lithvia@gmail.com>
2026-02-28 16:35:07 +0100

    Added schema generator

---
+++ src/uSupport.SchemaGenerator/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uSupport.Dtos.Settings;
+using System.Collections.Generic;
+
+namespace uSupport.SchemaGenerator
+{
+	internal class AppSettings
+	{
+		public uSupportSettings uSupport { get; set; } = new ();
+
+	}
+}
